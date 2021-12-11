0b66e65b79a74ba55ae94fe1e4d1244189243ff0
poonai <rbalajis25@gmail.com>
2021-12-11 14:38:43 +0530

    add policy evaluator
    Signed-off-by: poonai <rbalajis25@gmail.com>

---
+++ src/policy_evaluator/evaluator.rs
@@ -0,0 +1,152 @@
+// Copyright 2021 Balaji (rbalajis25@gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+use anyhow::{Error, Result};
+use burrego::opa::host_callbacks::DEFAULT_HOST_CALLBACKS;
+use burrego::opa::wasm::Evaluator;
+use serde_json::{Map, Value};
+use std::collections::HashMap;
+use tokio_postgres::types::Date;
+/// PolicyEvaluator is used to to evaluate policy decision for all the end user
+/// action.
+pub struct PolicyEvaluator {
+    evaluator: Evaluator,
+    entrypoints: HashMap<String, i32>,
+}
+
+pub struct PolicyResult {
+    pub allow: bool,
+    pub protected_columns: Vec<String>,
+}
+
+impl PolicyEvaluator {
+    // new returns policy evaluator to the caller.
+    pub fn new(policy: &Vec<u8>) -> Result<PolicyEvaluator, Error> {
+        let mut evaluator = Evaluator::new(
+            String::from("inspecktor-policy"),
+            policy,
+            &DEFAULT_HOST_CALLBACKS,
+        )?;
+        let mut entrypoints: HashMap<String, i32> = HashMap::default();
+        // find all the entrypoint ids. this will used while evaluating policies.
+        entrypoints.insert(
+            String::from("allow"),
+            evaluator.entrypoint_id(&"inspektor/resource/acl/allow")?,
+        );
+        entrypoints.insert(
+            String::from("protected_columns"),
+            evaluator.entrypoint_id(&"inspektor/resource/acl/protected_columns")?,
+        );
+        Ok(PolicyEvaluator {
+            evaluator,
+            entrypoints,
+        })
+    }
+
+    // evaluate will evaluate the policy for the given input.
+    pub fn evaluate(
+        &mut self,
+        data_source: &String,
+        groups: &Vec<String>,
+    ) -> Result<PolicyResult, anyhow::Error> {
+        let input = self.get_input_value(data_source, groups);
+        let data = Value::Object(Map::default());
+
+        let allow = self.evaluator.evaluate(
+            *self.entrypoints.get(&String::from("allow")).unwrap(),
+            &input,
+            &data,
+        )?;
+        let allow = self.get_result(allow);
+        let allow = match allow {
+            Value::Bool(allow) => allow,
+            _ => false,
+        };
+        if !allow {
+            return Ok(PolicyResult {
+                allow: false,
+                protected_columns: Vec::default(),
+            });
+        }
+        // get protected columns for the user.
+        let protected_columns = self.evaluator.evaluate(
+            *self
+                .entrypoints
+                .get(&String::from("protected_columns"))
+                .unwrap(),
+            &input,
+            &data,
+        )?;
+        let protected_columns = match self.get_result(protected_columns) {
+            Value::Array(vals) => vals
+                .into_iter()
+                .map(|i| match i {
+                    Value::String(s) => return s,
+                    _ => unreachable!("expected string"),
+                })
+                .collect::<Vec<String>>(),
+            _ => Vec::new(),
+        };
+        Ok(PolicyResult {
+            allow: allow,
+            protected_columns: protected_columns,
+        })
+    }
+
+    // get_result will returns result value from the policy value.
+    pub fn get_result(&self, value: serde_json::Value) -> Value {
+        if let Value::Array(mut objs) = value {
+            let obj = objs.remove(0);
+            if let Value::Object(mut obj) = obj {
+                return obj.remove("result").unwrap();
+            }
+            unreachable!("expected object");
+        }
+        unreachable!("expected array");
+    }
+
+    // get_input_value
+    fn get_input_value(&self, data_source: &String, groups: &Vec<String>) -> serde_json::Value {
+        let mut object = Map::with_capacity(2);
+        object.insert(
+            String::from("data_source"),
+            Value::String(data_source.clone()),
+        );
+        object.insert(
+            String::from("groups"),
+            Value::Array(
+                groups
+                    .iter()
+                    .map(|i| Value::String(i.clone()))
+                    .collect::<Vec<Value>>(),
+            ),
+        );
+        Value::Object(object)
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use std::fs;
+
+    use super::PolicyEvaluator;
+
+    #[test]
+    fn test_evaluator(){
+        let policy = fs::read("/home/poonai/inspektor/src/policy_evaluator/policy.wasm").unwrap();
+        let mut evaluator = PolicyEvaluator::new(&policy).unwrap();
+        let result = evaluator.evaluate(&String::from("postgres_production"), &vec![String::from("dev"), String::from("admin")]).unwrap();
+        assert_eq!(result.allow, true);
+        assert_eq!(result.protected_columns, vec![String::from("public.data_sources.side_car_token")]);
+    }
+}
