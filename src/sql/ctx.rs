5da40b2391bbfb9651625ef147f50d57cd420e47
poonai <rbalajis25@gmail.com>
2021-12-26 13:25:39 +0530

    remove all the dead code
    Signed-off-by: poonai <rbalajis25@gmail.com>

--- src/sql/ctx.rs
+++ src/sql/ctx.rs
@@ -1,6 +1,6 @@
 use futures::StreamExt;
 use md5::digest::generic_array::typenum::Len;
-use sqlparser::ast::{Expr, SelectItem, ObjectName};
+use sqlparser::ast::{Expr, ObjectName, SelectItem};
 use sqlparser::ast::{Ident, Value};
 use std::borrow::Cow;
 use std::collections::{BTreeMap, HashMap, HashSet};
@@ -9,45 +9,23 @@ use std::collections::{BTreeMap, HashMap, HashSet};
 // validating the selections.
 #[derive(Debug, Default, Clone)]
 pub struct Ctx {
-    // global_allowed_selections holds all the allowed column name for the entire query statement.
-    // eg: cte and processed sub query.
-    global_allowed_selections: HashMap<String, Vec<String>>,
-    // allowed_selection hold the selections that is allowed only for the current execution block.
-    allowed_selections: BTreeMap<String, Vec<String>>,
     //table info holds the informaion about the table.
     table_info: HashMap<String, Vec<String>>,
-
+    // protected_columns have the details of tables and their protected columns.
     protected_columns: HashMap<String, Vec<String>>,
-
+    // from contains the table names of the current selection/
     from: HashSet<String>,
 }
 
 impl Ctx {
+
     pub fn new(table_info: HashMap<String, Vec<String>>) -> Ctx {
         let mut state = Ctx::default();
         state.table_info = table_info;
         state
     }
-    pub fn insert_allowed_columns(&mut self, table_name: String, mut columns: Vec<String>) {
-        self.allowed_selections.insert(table_name, columns);
-    }
 
-    pub fn get_columns(&self, table_name: &String) -> Option<Vec<String>> {
-        if let Some(columns) = self.table_info.get(table_name) {
-            return Some(columns.clone());
-        }
-        None
-    }
-
-    pub fn get_default_table(&self) -> Option<String> {
-        if self.allowed_selections.len() == 1 {
-            for (key, _) in &self.allowed_selections {
-                return Some(key.to_string());
-            }
-        }
-        None
-    }
-
+    // is_allowed_column will tell the given column in allowed in the given table or not.
     pub fn is_allowed_column(&self, table_name: &String, column: &String) -> bool {
         if let Some(columns) = self.protected_columns.get(table_name) {
             return columns
@@ -58,69 +36,21 @@ impl Ctx {
         return true;
     }
 
+    // is_allowed_column_ident will tell the given column is allowed or not.
     pub fn is_allowed_column_ident(&self, column: &String) -> bool {
         let froms = self.from.clone().into_iter().collect::<Vec<String>>();
-        for from in &froms{
+        for from in &froms {
             if let Some(protected_columns) = self.protected_columns.get(from) {
-                match protected_columns.iter().position(|col| *col==*column) {
+                match protected_columns.iter().position(|col| *col == *column) {
                     Some(_) => return false,
-                    None => continue
+                    None => continue,
                 }
             }
         }
         return true;
     }
 
-    // is_valid_column check whether the given column is in the table info.
-    pub fn is_valid_column(&self, table_name: Option<&String>, column: &String) -> bool {
-        // if the table name given then check the column in valid for the given table name.
-        if let Some(table_name) = table_name {
-            let columns = match self.table_info.get(table_name) {
-                Some(columns) => columns,
-                _ => return false,
-            };
-            return columns
-                .iter()
-                .position(|table_column| -> bool { *table_column == *column })
-                .is_some();
-        }
-        // otherwise just iterate over all the table info.
-        for (_, columns) in &self.table_info {
-            match columns
-                .iter()
-                .position(|table_column| *table_column == *column)
-            {
-                Some(_) => return true,
-                None => continue,
-            };
-        }
-        return false;
-    }
-
-    pub fn get_allowed_columns(&self, table_name: &String) -> Option<Vec<String>> {
-        if let Some(columns) = self.allowed_selections.get(table_name) {
-            return Some(
-                columns
-                    .iter()
-                    .map(|c| c.to_string())
-                    .collect::<Vec<String>>(),
-            );
-        }
-        None
-    }
-
-    pub fn merge_table_info(&mut self, table_name: String, state: Ctx) {
-        for (_, val) in state.allowed_selections {
-            self.table_info.insert(table_name.clone(), val);
-        }
-    }
-
-    pub fn merge_allowed_selections(&mut self, table_name: String, state: Ctx) {
-        for (_, val) in state.allowed_selections {
-            self.allowed_selections.insert(table_name.clone(), val);
-        }
-    }
-
+    // merge_state will merget the incoming state with the current state.
     pub fn merge_state(&mut self, state: Ctx) {
         for (key, val) in state.protected_columns {
             self.protected_columns.insert(key, val);
@@ -133,26 +63,31 @@ impl Ctx {
         }
     }
 
+    // build_allowed_column_expr will returns all the allowed selection for the
+    // the current state.
     pub fn build_allowed_column_expr(&self) -> Vec<SelectItem> {
         let mut selections = vec![];
         let mut wildcard = true;
         let mut froms = self.from.clone().into_iter().collect::<Vec<String>>();
         froms.sort();
-        for from in froms{
+        for from in froms {
             let exprs = self.column_expr_for_table(&from);
             if exprs.len() == 0 {
-                selections.push(SelectItem::QualifiedWildcard(ObjectName(vec![Ident::new(from)])));
+                selections.push(SelectItem::QualifiedWildcard(ObjectName(vec![Ident::new(
+                    from,
+                )])));
                 continue;
             }
             wildcard = false;
-            selections.extend_from_slice(&exprs[..]);    
+            selections.extend_from_slice(&exprs[..]);
         }
         if wildcard {
-            return vec![SelectItem::Wildcard]
+            return vec![SelectItem::Wildcard];
         }
         return selections;
     }
 
+    // column_expr_for_table returns accepted columne expression for the given table.
     pub fn column_expr_for_table(&self, table_name: &String) -> Vec<SelectItem> {
         let mut selections = vec![];
         if let Some(protected_columns) = self.protected_columns.get(table_name) {
@@ -162,7 +97,10 @@ impl Ctx {
                 if protected_columns_set.contains(col) {
                     selections.push(SelectItem::ExprWithAlias {
                         expr: Expr::Value(Value::Null),
-                        alias:Ident { value: format!("{}.{}", table_name, col), quote_style: Some('"')},
+                        alias: Ident {
+                            value: format!("{}.{}", table_name, col),
+                            quote_style: Some('"'),
+                        },
                     });
                     continue;
                 }
@@ -175,6 +113,7 @@ impl Ctx {
         return selections;
     }
 
+    // memorize_protected_columns insert protected column to the state.
     pub fn memorize_protected_columns(
         &mut self,
         table_name: String,
@@ -183,6 +122,7 @@ impl Ctx {
         self.protected_columns.insert(table_name, protected_columns);
     }
 
+    // get_protected_columns returns protected columns for the given table.
     pub fn get_protected_columns(&self, table_name: &String) -> Option<Vec<String>> {
         if let Some(protected_columns) = self.protected_columns.get(table_name) {
             return Some(
@@ -195,12 +135,7 @@ impl Ctx {
         return None;
     }
 
-    pub fn merge_protected_columns(&mut self, alias_name: String, state: Ctx) {
-        for (_, val) in state.protected_columns {
-            self.protected_columns.insert(alias_name.clone(), val);
-        }
-    }
-
+    // overwrite_table_info will rewrite the src table name with the given alias name.
     pub fn overwrite_table_info(&mut self, table_name: &String, alias: String) {
         if let Some(columns) = self.table_info.get(table_name) {
             self.table_info.insert(alias, columns.clone());
@@ -209,6 +144,7 @@ impl Ctx {
         self.table_info.insert(alias, vec![]);
     }
 
+    // add_from_src inserts from table
     pub fn add_from_src(&mut self, table_name: String) {
         self.from.insert(table_name);
     }
