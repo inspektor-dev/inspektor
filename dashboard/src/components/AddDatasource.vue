fbf6746bbdfb186a127b3dfc795f9043454fa2e4
password <rbalajis25@gmail.com>
2022-01-01 14:06:47 +0530

    add form validation for add datasource component
    Signed-off-by: password <rbalajis25@gmail.com>

--- dashboard/src/components/AddDatasource.vue
+++ dashboard/src/components/AddDatasource.vue
@@ -1,36 +1,90 @@
 <template>
   <div>
-
-    <n-form-item path="bane" label="Datasource name">
-      <n-input />
-    </n-form-item>
-    <n-form :model="model">
-      <n-form-item label="Datasource Type" path="type">
-        <n-select placeholder="Select" :options="generalOptions" />
+    <n-form :model="formValue" :rules="rules" ref="formRef">
+      <n-form-item path="name" label="Datasource name">
+        <n-input v-model:value="formValue.name" placeholder="prod postgres" />
       </n-form-item>
-      <n-form-item path="roles" label="roles">
-        <n-dynamic-tags v-model:value="roles" />
+      <n-form-item label="Datasource Type" path="type">
+        <n-select
+          placeholder="Select datasource type"
+          :options="generalOptions"
+          v-model:value="formValue.type"
+        />
+      </n-form-item>
+      <n-form-item
+        path="roles"
+        label="roles"
+        :validation-status="validateRoles"
+        :feedback="feedbackForRolesValidation"
+      >
+        <n-dynamic-tags v-model:value="formValue.roles" />
       </n-form-item>
       <div style="display: flex; justify-content: flex-end">
-        <n-button type="success">Create Datasource</n-button>
+        <n-button type="success" @click="createDatasource"
+          >Create Datasource</n-button
+        >
       </div>
     </n-form>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import { useMessage } from "naive-ui";
 
 export default {
   setup() {
     //let roles = ref(['admin']);
+    let formRef = ref(null);
+    const message = useMessage();
+    let formValue = ref({
+      name: "",
+      type: "",
+      roles: ["admin"],
+    });
     return {
+      formRef,
+      formValue,
+      rules: {
+        name: {
+          required: true,
+          message: "please enter datasource name",
+          trigger: "blur",
+        },
+        type: {
+          required: true,
+          message: "please select datasource type",
+          trigger: "blur",
+        },
+      },
       generalOptions: ["Postgres"].map((v) => ({
         label: v,
         value: v,
       })),
-      //  roles: roles,
-      roles: ref(["admin"]),
+      validateRoles: computed(() => {
+        if (formValue.value.roles.length == 0) {
+          return "error";
+        }
+        return undefined;
+      }),
+      feedbackForRolesValidation: computed(() => {
+        if (formValue.value.roles.length == 0) {
+          return "enter roles for datasource";
+        }
+        return "";
+      }),
+      createDatasource(e) {
+        e.preventDefault();
+        formRef.value.validate((errors) => {
+          if (errors) {
+            message.error("Invalid data");
+            return;
+          }
+          if (formValue.value.roles.length == 0) {
+            message.error("Enter roles for datasource");
+          }
+        });
+      },
     };
   },
   name: "AddDatasource",
