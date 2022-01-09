121fe6bbe962786a8711b8bf4a56b20b7c7819da
password <rbalajis25@gmail.com>
2022-01-09 12:06:45 +0530

    add skeleton for demo setup
    Signed-off-by: password <rbalajis25@gmail.com>

--- build_controlplane_docker.sh
+++ build_controlplane_docker.sh
@@ -9,4 +9,4 @@ npm run build
 cd ../..
 
 
-docker build .  -t schoolboy/inspektor-controlplane -f Dockerfile.controlplane:latest
+docker build .  -t schoolboy/inspektor-controlplane:latest -f Dockerfile.controlplane
