f8235c7db84350a80f97d88ecc51cafc1929c3c1
password <rbalajis25@gmail.com>
2022-01-09 11:44:10 +0530

    fix handling spa
    Signed-off-by: password <rbalajis25@gmail.com>

--- controlplane/handlers/user.go
+++ controlplane/handlers/user.go
@@ -9,6 +9,8 @@ import (
 	"inspektor/types"
 	"inspektor/utils"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/golang-jwt/jwt"
@@ -147,6 +149,49 @@ func (h *Handlers) Roles() InspectorHandler {
 	}
 }
 
+// spaHandler implements the http.Handler interface, so we can use it
+// to respond to HTTP requests. The path to the static directory and
+// path to the index file within that static directory are used to
+// serve the SPA in the given static directory.
+type spaHandler struct {
+	staticPath string
+	indexPath  string
+}
+
+// ServeHTTP inspects the URL path to locate a file within the static dir
+// on the SPA handler. If a file is found, it will be served. If not, the
+// file located at the index path on the SPA handler will be served. This
+// is suitable behavior for serving an SPA (single page application).
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path, err := filepath.Abs(r.URL.Path)
+	if err != nil {
+		// if we failed to get the absolute path respond with a 400 bad request
+		// and stop
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// prepend the path with the path to the static directory
+	path = filepath.Join(h.staticPath, path)
+
+	// check whether a file exists at the given path
+	_, err = os.Stat(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		http.ServeFile(w, r, filepath.Join(h.staticPath, h.indexPath))
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.Dir(h.staticPath)).ServeHTTP(w, r)
+}
+
 func (h *Handlers) Init(router *mux.Router) {
 	router.HandleFunc("/api/login", h.Login()).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/datasource", h.AuthMiddleWare(h.CreateDataSource())).Methods("POST", "OPTIONS")
@@ -157,8 +202,8 @@ func (h *Handlers) Init(router *mux.Router) {
 	router.HandleFunc("/api/user", h.AuthMiddleWare(h.AddUser())).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/users", h.AuthMiddleWare(h.GetUsers())).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/roles", h.AuthMiddleWare(h.Roles())).Methods("GET", "OPTIONS")
-	fileServer := http.FileServer(http.Dir("./dashboard/dist"))
-	router.PathPrefix("/").Handler(http.StripPrefix("/", fileServer))
+	spa := spaHandler{staticPath: "dashboard/dist", indexPath: "index.html"}
+	router.PathPrefix("/").Handler(spa)
 	cors := handlers.CORS(
 		handlers.AllowedHeaders([]string{"Content-Type", "Auth-Token"}),
 		handlers.AllowedOrigins([]string{"*"}),
