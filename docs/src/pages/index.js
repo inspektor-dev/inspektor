1ffd31d504d87ba09773be8047a53e429ef36456
password <rbalajis25@gmail.com>
2022-01-23 12:14:05 +0530

    add facvico
    Signed-off-by: password <rbalajis25@gmail.com>

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title="Inpektor - Open Policy for your data layer"
+      description="Protect you datasources using open policy agent">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
