9798940d9cfa8ed94b55f7a2b77003672cf3547a
password <rbalajis25@gmail.com>
2022-04-01 15:01:37 +0530

    add private channel id to the config

--- controlplane/slackbot/slack.go
+++ controlplane/slackbot/slack.go
@@ -19,28 +19,34 @@ import (
 	"inspektor/config"
 	"inspektor/utils"
 
+	"github.com/google/uuid"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
 	"github.com/slack-go/slack/socketmode"
 	"go.uber.org/zap"
 )
 
+type AccessRequest struct {
+	UserID string
+}
 type SlackBot struct {
-	client *slack.Client
+	client          *slack.Client
+	adminChannelID  string
+	pendingRequests map[string]AccessRequest
 }
 
 func New(cfg *config.Config) *SlackBot {
 	client := slack.New(cfg.SlackBotToken,
 		slack.OptionAppLevelToken(cfg.SlackAppToken), slack.OptionDebug(true))
 	return &SlackBot{
-		client: client,
+		client:         client,
+		adminChannelID: cfg.SlackAdminChannelID,
 	}
 }
 
 func (s *SlackBot) Start() {
 	socketClient := socketmode.New(s.client, socketmode.OptionDebug(true))
 	go func() {
-		fmt.Println("socket listen")
 		for event := range socketClient.Events {
 			switch event.Type {
 			case socketmode.EventTypeEventsAPI:
@@ -51,11 +57,60 @@ func (s *SlackBot) Start() {
 				}
 				s.handleEventApi(event.InnerEvent)
 			case socketmode.EventTypeInteractive:
-				action, ok := event.Data.(slack.InteractionCallback)
+				interactiveEvent, ok := event.Data.(slack.InteractionCallback)
 				if !ok {
 					continue
 				}
-				s.handleActionCB(action.ActionCallback, action.TriggerID)
+				switch interactiveEvent.Type {
+				case slack.InteractionTypeBlockActions:
+					for _, cb := range interactiveEvent.ActionCallback.BlockActions {
+						switch cb.ActionID {
+						case "request-access":
+							databases := NewBlockOptions([]string{"databases"}, []string{"databases"})
+							roles := NewBlockOptions([]string{"admin", "dev"}, []string{"admin", "dev"})
+							_, err := s.client.OpenView(interactiveEvent.TriggerID,
+								NewRequestAccessModal(uuid.NewString(), databases, roles))
+							if err != nil {
+								utils.Logger.Error("error while opening modal view", zap.String("err_msg", err.Error()))
+							}
+							fmt.Println("modal triggered")
+						default:
+							socketClient.Ack(*event.Request)
+							_, _, err := s.client.PostMessage(s.adminChannelID, slack.MsgOptionText("Thanks for approving. Credentials will be sent to the user", false))
+							if err != nil {
+								utils.Logger.Error("error while approval message to the admin", zap.String("err_msg", err.Error()))
+							}
+						}
+					}
+				case slack.InteractionTypeViewSubmission:
+					fmt.Println("callback id ", interactiveEvent.View.CallbackID)
+					fmt.Println("raw state\n\n\n")
+					fmt.Println(interactiveEvent.RawState)
+					fmt.Printf("%+v", interactiveEvent.View.State.Values)
+					databaseState, ok := interactiveEvent.View.State.Values["databaseblock"]["database"]
+					if !ok {
+						utils.Logger.Error("unable to find databaseblock on view submission")
+						continue
+					}
+					rolesState, ok := interactiveEvent.View.State.Values["rolesblock"]["roles"]
+					if !ok {
+						utils.Logger.Error("unable to find rolesblock on view submission")
+						continue
+					}
+					socketClient.Ack(*event.Request)
+					fmt.Println("selected database", databaseState.SelectedOption.Value)
+					fmt.Println("selected roles", rolesState.SelectedOptions)
+					_, err := s.client.PublishView(interactiveEvent.User.ID, NewRequestSentView(), "")
+					if err != nil {
+						utils.Logger.Error("error while publishing request sent view", zap.String("err_msg", err.Error()))
+					}
+					_, _, err = s.client.PostMessage(s.adminChannelID,
+						slack.MsgOptionBlocks(NewRequestAccessMsg("ppoami", "postgres", []string{"admin", "dev"})...))
+					if err != nil {
+						utils.Logger.Error("error while sending approval message", zap.String("err_msg", err.Error()))
+					}
+				}
+
 			default:
 				utils.Logger.Debug("skipping slack event", zap.String("event_name", string(event.Type)))
 			}
@@ -64,188 +119,17 @@ func (s *SlackBot) Start() {
 	socketClient.Run()
 }
 
-func (s *SlackBot) handleActionCB(cbs slack.ActionCallbacks, triggerID string) {
-	for _, cb := range cbs.BlockActions {
-		switch cb.ActionID {
-		case "request-access":
-			_, err := s.client.OpenView(triggerID, slack.ModalViewRequest{
-				Type: slack.VTModal,
-				Title: &slack.TextBlockObject{
-					Type: slack.PlainTextType,
-					Text: "Request Temporary Access",
-				},
-				Submit: &slack.TextBlockObject{
-					Type: slack.PlainTextType,
-					Text: "Request Access",
-				},
-				Close: &slack.TextBlockObject{
-					Type: slack.PlainTextType,
-					Text: "Cancel",
-				},
-				Blocks: slack.Blocks{
-					BlockSet: []slack.Block{
-						slack.SectionBlock{
-							Type: slack.MBTSection,
-							Text: &slack.TextBlockObject{
-								Type: slack.PlainTextType,
-								Text: "Select database you wish to access",
-							},
-							Accessory: &slack.Accessory{
-								SelectElement: &slack.SelectBlockElement{
-									Type: slack.OptTypeStatic,
-									Placeholder: &slack.TextBlockObject{
-										Type: slack.PlainTextType,
-										Text: "Select a Database",
-									},
-									Options: []*slack.OptionBlockObject{
-										&slack.OptionBlockObject{
-											Text: &slack.TextBlockObject{
-												Type: slack.PlainTextType,
-												Text: "postgres-prod",
-											},
-											Value: "postgres-prod",
-										},
-										&slack.OptionBlockObject{
-											Text: &slack.TextBlockObject{
-												Type: slack.PlainTextType,
-												Text: "postgres-prod1",
-											},
-											Value: "postgres-prod1",
-										},
-										&slack.OptionBlockObject{
-											Text: &slack.TextBlockObject{
-												Type: slack.PlainTextType,
-												Text: "postgres-prod2",
-											},
-											Value: "postgres-prod2",
-										},
-									},
-								},
-							},
-						},
-						slack.SectionBlock{
-							Type: slack.MBTSection,
-							Text: &slack.TextBlockObject{
-								Type: slack.PlainTextType,
-								Text: "Select roles you wish to obtain",
-							},
-							Accessory: &slack.Accessory{
-								SelectElement: &slack.SelectBlockElement{
-									Type: slack.MultiOptTypeStatic,
-									Placeholder: &slack.TextBlockObject{
-										Type: slack.PlainTextType,
-										Text: "Select roles",
-									},
-									Options: []*slack.OptionBlockObject{
-										&slack.OptionBlockObject{
-											Text: &slack.TextBlockObject{
-												Type: slack.PlainTextType,
-												Text: "admin",
-											},
-											Value: "admin",
-										},
-										&slack.OptionBlockObject{
-											Text: &slack.TextBlockObject{
-												Type: slack.PlainTextType,
-												Text: "support",
-											},
-											Value: "support",
-										},
-										&slack.OptionBlockObject{
-											Text: &slack.TextBlockObject{
-												Type: slack.PlainTextType,
-												Text: "dev",
-											},
-											Value: "dev",
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			})
-			if err != nil {
-				utils.Logger.Error("error while opening modal view", zap.String("err_msg", err.Error()))
-			}
-			fmt.Println("modal triggered")
-		}
-	}
-}
-
 func (s *SlackBot) handleEventApi(data interface{}) {
-	fmt.Println("event api\n\n\n\n\n\n\n\n\n\n\n\n\n")
-	fmt.Println(string(utils.MarshalJSON(data)))
 	switch event := data.(type) {
 	case slackevents.EventsAPIInnerEvent:
 		innerEvent, ok := event.Data.(*slackevents.AppHomeOpenedEvent)
 		if !ok {
 			return
 		}
-		fmt.Println("publishing vent")
-		// {
-		// 	"blocks": [
-		// 		{
-		// 			"type": "section",
-		// 			"text": {
-		// 				"type": "mrkdwn",
-		// 				"text": "Hi👋, I'm Inspektor bot 🤖. you can request for \n database access through me"
-		// 			},
-		// 			"accessory": {
-		// 				"type": "button",
-		// 				"text": {
-		// 					"type": "plain_text",
-		// 					"text": "Request Access",
-		// 					"emoji": true
-		// 				},
-		// 				"value": "click_me_123",
-		// 				"action_id": "button-action"
-		// 			}
-		// 		}
-		// 	]
-		// }
-		// slack.ActionBlock{
-		// 	Type: slack.MBTAction,
-		// 	Elements: &slack.BlockElements{
-		// 		[]slack.BlockElement{
-		// 			slack.ButtonBlockElement{
-		// 				Type: slack.METButton,
-		// 				Text: &slack.TextBlockObject{
-		// 					Type: slack.PlainTextType,
-		// 					Text: "Request Access",
-		// 				},
-		// 			},
-		// 		},
-		// 	},
-		// },
-		res, err := s.client.PublishView(innerEvent.User, slack.HomeTabViewRequest{
-			Type: slack.VTHomeTab,
-			Blocks: slack.Blocks{[]slack.Block{
-				slack.SectionBlock{
-					Type: slack.MBTSection,
-					Text: &slack.TextBlockObject{
-						Type: slack.MarkdownType,
-						Text: "Hi👋, I'm Inspektor bot 🤖. you can request for database access through me",
-					},
-					Accessory: &slack.Accessory{
-						ButtonElement: &slack.ButtonBlockElement{
-							Type: slack.METButton,
-							Text: &slack.TextBlockObject{
-								Type:  slack.PlainTextType,
-								Text:  "Request Access",
-								Emoji: true,
-							},
-							Value:    "access_requested",
-							ActionID: "request-access",
-						},
-					},
-				},
-			}},
-		}, "")
+		_, err := s.client.PublishView(innerEvent.User, NewHomeTabView(), "")
 		if err != nil {
-			utils.Logger.Error("error whule publishing view", zap.String("err_msg", err.Error()))
+			utils.Logger.Error("error while publishing view", zap.String("err_msg", err.Error()))
 			return
 		}
-		utils.Logger.Debug("view response", zap.String("res", string(utils.MarshalJSON(res))))
 	}
 }
