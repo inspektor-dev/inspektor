f3111f1d82422a16f3c5f5ee354b40d411729c20
poonai <rbalajis25@gmail.com>
2022-04-02 19:44:59 +0530

    refactor slack bot

--- controlplane/slackbot/slack.go
+++ controlplane/slackbot/slack.go
@@ -29,7 +29,7 @@ import (
 )
 
 type AccessRequest struct {
-	UserID   string
+	User     *slack.User
 	database uint
 	Roles    []string
 }
@@ -38,11 +38,13 @@ type SlackBot struct {
 	adminChannelID  string
 	pendingRequests map[string]AccessRequest
 	store           *store.Store
+	socketClient    *socketmode.Client
 }
 
+// New will return slack bot.
 func New(cfg *config.Config, store *store.Store) *SlackBot {
 	client := slack.New(cfg.SlackBotToken,
-		slack.OptionAppLevelToken(cfg.SlackAppToken), slack.OptionDebug(true))
+		slack.OptionAppLevelToken(cfg.SlackAppToken), slack.OptionDebug(false))
 	return &SlackBot{
 		client:          client,
 		adminChannelID:  cfg.SlackAdminChannelID,
@@ -51,10 +53,12 @@ func New(cfg *config.Config, store *store.Store) *SlackBot {
 	}
 }
 
+// Start will start listening for websocket event and handles all the interative events.
+// eg: request submission and credentials delivery.
 func (s *SlackBot) Start() {
-	socketClient := socketmode.New(s.client, socketmode.OptionDebug(true))
+	s.socketClient = socketmode.New(s.client, socketmode.OptionDebug(false))
 	go func() {
-		for event := range socketClient.Events {
+		for event := range s.socketClient.Events {
 			switch event.Type {
 			case socketmode.EventTypeEventsAPI:
 				event, ok := event.Data.(slackevents.EventsAPIEvent)
@@ -71,100 +75,12 @@ func (s *SlackBot) Start() {
 				switch interactiveEvent.Type {
 				case slack.InteractionTypeBlockActions:
 					for _, cb := range interactiveEvent.ActionCallback.BlockActions {
-						switch cb.ActionID {
-						case "request-access":
-							datasources, err := s.store.GetDataSource()
-							if err != nil {
-								utils.Logger.Error("error while retriving all the datasources", zap.String("err_msg", err.Error()))
-								continue
-							}
-							databaseNames := []string{}
-							databaseIDs := []string{}
-							for _, datasource := range datasources {
-								databaseNames = append(databaseNames, datasource.Name)
-								databaseIDs = append(databaseIDs, fmt.Sprintf("%d", datasource.ID))
-							}
-							databases := NewBlockOptions(databaseNames, databaseIDs)
-							roles, err := s.store.GetRoles()
-							if err != nil {
-								utils.Logger.Error("error while retriving roles", zap.String("err_msg", err.Error()))
-								continue
-							}
-							rolesBlock := NewBlockOptions(roles, roles)
-							modalView := NewRequestAccessModal(uuid.NewString(), databases, rolesBlock)
-							fmt.Print("\n\n\n\n")
-							fmt.Println(string(utils.MarshalJSON(modalView)))
-							_, err = s.client.OpenView(interactiveEvent.TriggerID, modalView)
-							if err != nil {
-								utils.Logger.Error("error while opening modal view", zap.String("err_msg", err.Error()))
-							}
-							fmt.Println("modal triggered")
-						case "denied":
-							socketClient.Ack(*event.Request)
-							_, _, err := s.client.PostMessage(s.adminChannelID, slack.MsgOptionText("We'll let them know that the request has been denied", false))
-							if err != nil {
-								utils.Logger.Error("error while approval message to the admin", zap.String("err_msg", err.Error()))
-							}
-						case "approved":
-							socketClient.Ack(*event.Request)
-							accessRequest, ok := s.pendingRequests[interactiveEvent.View.CallbackID]
-							if !ok {
-								_, _, err := s.client.PostMessage(s.adminChannelID, slack.MsgOptionText("Unable to find the request", false))
-								if err != nil {
-									utils.Logger.Error("error while approval message to the admin", zap.String("err_msg", err.Error()))
-								}
-								continue
-							}
-							// apporve request
-							fmt.Println(accessRequest)
-							// send credentials.
-							_, _, err := s.client.PostMessage(s.adminChannelID, slack.MsgOptionText("Thanks for approving. Credentials will be sent to the user", false))
-							if err != nil {
-								utils.Logger.Error("error while approval message to the admin", zap.String("err_msg", err.Error()))
-							}
-						}
+						s.socketClient.Ack(*event.Request)
+						s.handleAction(cb, interactiveEvent)
 					}
 				case slack.InteractionTypeViewSubmission:
-					fmt.Println("callback id ", interactiveEvent.View.CallbackID)
-					fmt.Println("raw state\n\n\n")
-					fmt.Println(interactiveEvent.RawState)
-					fmt.Printf("%+v", interactiveEvent.View.State.Values)
-					databaseState, ok := interactiveEvent.View.State.Values["databaseblock"]["database"]
-					if !ok {
-						utils.Logger.Error("unable to find databaseblock on view submission")
-						continue
-					}
-					rolesState, ok := interactiveEvent.View.State.Values["rolesblock"]["roles"]
-					if !ok {
-						utils.Logger.Error("unable to find rolesblock on view submission")
-						continue
-					}
-					socketClient.Ack(*event.Request)
-					fmt.Println("selected database", databaseState.SelectedOption.Value)
-					selectedDatabase, err := strconv.Atoi(databaseState.SelectedOption.Value)
-					if err != nil {
-						utils.Logger.Error("error while parsinsg database id", zap.String("err_msg", err.Error()))
-						continue
-					}
-					selectedRoles := []string{}
-					for _, option := range rolesState.SelectedOptions {
-						selectedRoles = append(selectedRoles, option.Value)
-					}
-					fmt.Println("selected roles", rolesState.SelectedOptions)
-					_, err = s.client.PublishView(interactiveEvent.User.ID, NewRequestSentView(), "")
-					if err != nil {
-						utils.Logger.Error("error while publishing request sent view", zap.String("err_msg", err.Error()))
-					}
-					_, _, err = s.client.PostMessage(s.adminChannelID,
-						slack.MsgOptionBlocks(NewRequestAccessMsg(interactiveEvent.User.ID, databaseState.SelectedOption.Text.Text, selectedRoles, interactiveEvent.View.CallbackID)...))
-					if err != nil {
-						utils.Logger.Error("error while sending approval message", zap.String("err_msg", err.Error()))
-					}
-					s.pendingRequests[interactiveEvent.View.CallbackID] = AccessRequest{
-						UserID:   interactiveEvent.User.ID,
-						database: uint(selectedDatabase),
-						Roles:    selectedRoles,
-					}
+					s.socketClient.Ack(*event.Request)
+					s.handleRequestSubmission(interactiveEvent)
 				}
 
 			default:
@@ -172,9 +88,10 @@ func (s *SlackBot) Start() {
 			}
 		}
 	}()
-	socketClient.Run()
+	s.socketClient.Run()
 }
 
+// handleEventApi shows entry point for the user to intiate request access.
 func (s *SlackBot) handleEventApi(data interface{}) {
 	switch event := data.(type) {
 	case slackevents.EventsAPIInnerEvent:
@@ -187,5 +104,134 @@ func (s *SlackBot) handleEventApi(data interface{}) {
 			utils.Logger.Error("error while publishing view", zap.String("err_msg", err.Error()))
 			return
 		}
+	}
+}
+
+// handleRequestSubmission will handle the form submission event. We do get this event only if
+// the user submitted request access modal.
+func (s *SlackBot) handleRequestSubmission(iEvent slack.InteractionCallback) {
+	// retrive the value of datsource id and roles which they want to gain.
+	databaseState, ok := iEvent.View.State.Values["databaseblock"]["database"]
+	if !ok {
+		utils.Logger.Error("unable to find databaseblock on view submission")
+		return
+	}
+	rolesState, ok := iEvent.View.State.Values["rolesblock"]["roles"]
+	if !ok {
+		utils.Logger.Error("unable to find rolesblock on view submission")
+		return
+	}
+	selectedDatabase, err := strconv.Atoi(databaseState.SelectedOption.Value)
+	if err != nil {
+		utils.Logger.Error("error while parsinsg database id", zap.String("err_msg", err.Error()))
+		return
+	}
+	selectedRoles := []string{}
+	for _, option := range rolesState.SelectedOptions {
+		selectedRoles = append(selectedRoles, option.Value)
+	}
+
+	// track the request in the memory, so if the admin approve we can map the approval
+	// with the request and can send credentials to the user.
+	user, err := s.client.GetUserInfo(iEvent.User.ID)
+	if err != nil {
+		utils.Logger.Error("error while retriving user details", zap.String("err_msg", err.Error()))
+		return
+	}
+	s.pendingRequests[iEvent.View.CallbackID] = AccessRequest{
+		User:     user,
+		database: uint(selectedDatabase),
+		Roles:    selectedRoles,
+	}
+	// send approval view for the admin to approve or deny the request.
+	_, err = s.client.PublishView(iEvent.User.ID, NewRequestSentView(), "")
+	if err != nil {
+		utils.Logger.Error("error while publishing request sent view", zap.String("err_msg", err.Error()))
+	}
+	_, _, err = s.client.PostMessage(s.adminChannelID,
+		slack.MsgOptionBlocks(NewRequestAccessMsg(user.Name, databaseState.SelectedOption.Text.Text, selectedRoles, iEvent.View.CallbackID)...))
+	if err != nil {
+		utils.Logger.Error("error while sending approval message", zap.String("err_msg", err.Error()))
+	}
+
+}
+
+func (s *SlackBot) handleAction(action *slack.BlockAction, iEvent slack.InteractionCallback) {
+	switch action.ActionID {
+	case "request-access":
+		// access has been requested. So, open a modal where user can choose
+		// the database and roles which they want to gain.
+		datasources, err := s.store.GetDataSource()
+		if err != nil {
+			utils.Logger.Error("error while retriving all the datasources", zap.String("err_msg", err.Error()))
+			return
+		}
+		databaseNames := []string{}
+		databaseIDs := []string{}
+		for _, datasource := range datasources {
+			databaseNames = append(databaseNames, datasource.Name)
+			databaseIDs = append(databaseIDs, fmt.Sprintf("%d", datasource.ID))
+		}
+		roles, err := s.store.GetRoles()
+		if err != nil {
+			utils.Logger.Error("error while retriving roles", zap.String("err_msg", err.Error()))
+			return
+		}
+
+		// create the modal view for the user.
+		rolesBlock := NewBlockOptions(roles, roles)
+		databases := NewBlockOptions(databaseNames, databaseIDs)
+		modalView := NewRequestAccessModal(uuid.NewString(), databases, rolesBlock)
+		_, err = s.client.OpenView(iEvent.TriggerID, modalView)
+		if err != nil {
+			utils.Logger.Error("error while opening modal view", zap.String("err_msg", err.Error()))
+		}
+	case "denied":
+		// request has been denied, so let the requested user know that the request has been
+		// denied.
+		s.postMessage(s.adminChannelID, "We'll let them know that the request has been denied")
+		accessRequest, ok := s.pendingRequests[action.Value]
+		if !ok {
+			return
+		}
+		s.postMessage(accessRequest.User.ID, "Your access request has been denied")
+	case "approved":
+		// request has been approved, so create the temp session and send the credentials as direct message
+		// to the requested user.
+		accessRequest, ok := s.pendingRequests[action.Value]
+		if !ok {
+			_, _, err := s.client.PostMessage(s.adminChannelID, slack.MsgOptionText("Unable to find the request", false))
+			if err != nil {
+				utils.Logger.Error("error while approval message to the admin", zap.String("err_msg", err.Error()))
+			}
+			return
+		}
+		session, err := s.store.CreateTempSession(accessRequest.database, accessRequest.Roles, 10, "slack", utils.MarshalJSON(accessRequest.User))
+		if err != nil {
+			utils.Logger.Error("error while creating temp credentials", zap.String("err_msg", err.Error()))
+			return
+		}
+		datasource, err := s.store.GetDatasource(accessRequest.database)
+		if err != nil {
+			utils.Logger.Error("error while retirving datasource", zap.String("err_msg", err.Error()))
+			s.postMessage(s.adminChannelID, "unable to retrive datasoruce information")
+			return
+		}
+		credentialBlock := NewCredentialsBlock(session.SessionMeta.PostgresUsername, session.SessionMeta.PostgresPassword, datasource.SideCarHostName)
+		_, _, err = s.client.PostMessage(accessRequest.User.ID, slack.MsgOptionBlocks(credentialBlock...))
+		if err != nil {
+			utils.Logger.Error("error while sending credentials to the user")
+			s.postMessage(s.adminChannelID, "error while sending credentials to the user")
+			return
+		}
+		s.postMessage(s.adminChannelID, "Thanks for approving. Credentials will be sent to the user")
+	}
+}
+
+// postMessage will send plain text message to the given channel id.
+func (s *SlackBot) postMessage(channelID string, msg string) {
+	_, _, err := s.client.PostMessage(channelID, slack.MsgOptionText(msg, false))
+	if err != nil {
+		utils.Logger.Error("error while approval message to the admin", zap.String("err_msg", err.Error()))
 	}
 }
