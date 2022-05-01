8f3e07248d96a8c5a1fdd8ab7b5d3452b2bc8cf9
poonai <rbalajis25@gmail.com>
2022-05-01 17:28:15 +0530

    ux improved
    Signed-off-by: poonai <rbalajis25@gmail.com>

--- controlplane/dashboard/src/main.js
+++ controlplane/dashboard/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import naive from 'naive-ui'
 import store from './store/store'
-
+ 
 let app = createApp(App)
     app.use(router).
     use(store).
