794e06b29254bd759cc92daadd9960c2f904bd9f
poonai <rbalajis25@gmail.com>
2022-07-10 19:07:24 +0530

    upgrade dependencies and change the protobuf building from grpc-rs to tonic

--- build.rs
+++ build.rs
@@ -1,8 +1,7 @@
-use protoc_grpcio;
 
-fn main() {
-    let proto_root = "proto";
-    println!("cargo:rerun-if-changed={}", proto_root);
-    protoc_grpcio::compile_grpc_protos(&["api.proto"], &[proto_root], "src/apiproto", None)
-        .expect("Failed to compile gRPC definitions!");
+use std::io::Result;
+
+fn main() -> Result<()> {
+    tonic_build::compile_protos("proto/api.proto")?;
+    Ok(())
 }
